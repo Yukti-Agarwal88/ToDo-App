95447bac4cdb0655dc4fdb27223157aca64b7ba1
Yukti <yukti.agarwal08@gmail.com>
2021-05-29 18:32:18 +0530

    To Do Commit

---
+++ app/src/main/java/com/yukti/android/todoapp/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.yukti.android.todoapp;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
---
+++ app/src/main/java/com/yukti/android/todoapp/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.yukti.android.todoapp;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(SplashActivity.this,R.color.black));
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
